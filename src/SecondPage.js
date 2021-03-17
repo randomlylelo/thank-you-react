6514770c348e5d887dbae89ed89a8b7490138065
Abraham Belayneh <epicabe145@gmail.com>
2021-03-17 12:36:24 -0400

    added router from view update

---
+++ src/SecondPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SecondPage() {
+  return (
+    <div className="p-40 mx-auto max-w-lh bg-blue-200">
+      <img
+        src="https://i.pinimg.com/originals/e7/43/27/e7432700100cf0f778bfbaf364947b91.png"
+        alt=""
+      />
+    </div>
+  );
+}

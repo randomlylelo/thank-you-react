763a866b7bbb80f0c6947d8c80916a4902e0ce7b
Eric P <eric.1334@icloud.com>
2021-03-17 13:05:13 -0700

    Make confetti exactly the same

--- src/SecondPage.js
+++ src/SecondPage.js
@@ -1,45 +1,12 @@
 import React from "react";
-import ConfettiGenerator from "confetti-js";
+import confetti from "./confetti.js";
 
 export default function SecondPage() {
-  const [dimensions, setDimensions] = React.useState({
-    height: window.innerHeight,
-    width: window.innerWidth
-  });
-
-  // update state on resize so that the confetti always covers the whole screen
   React.useEffect(() => {
-    const handleResize = () => {
-      setDimensions({
-        height: window.innerHeight,
-        width: window.innerWidth
-      });
-    }
-    
-    window.addEventListener('resize', handleResize);
-    
-    return _ => { window.removeEventListener('resize', handleResize) };
-  });
-
-  React.useEffect(() => {
-    const confettiSettings = {target: 'canvas', colors: [[255,255,255], [255,108,12]]};
-    const confetti = new ConfettiGenerator(confettiSettings);
-    confetti.render();
-
-    return () => confetti.clear();
-  }, [])
-
-  const canvasStyle = {
-    position: "fixed",
-    top: "0",
-    left: "0",
-    width: dimensions.width,
-    height: dimensions.height
-  };
-
+      confetti({ colors: [ [ '#fe8c00', '#ffffff' ], [ '#fe8c00', '#ffffff' ] ], timeout: 25 });
+  }, []);
   return (
     <div className="p-40 mx-auto max-w-lh bg-blue-200">
-      <canvas id="canvas" style={canvasStyle}></canvas>
       <img
         src="https://i.pinimg.com/originals/e7/43/27/e7432700100cf0f778bfbaf364947b91.png"
         alt=""
